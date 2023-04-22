91ff9da3b194e9db372066018a5ad28a32c3e85f
vladya905 <lubajkinvlad@gmail.com>
2023-04-22 15:45:13 +0300

    commit2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,46 +3,44 @@ import { galleryItems } from './gallery-items.js';
 const galleryContainer = document.querySelector('.gallery');
 const addCreateElement = createItemsEl(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', addCreateElement);
-galleryContainer.addEventListener('click', onHrefClick)
-function onHrefClick(event) {
-  event.preventDefault();
-  const { target } = event;
-  if (target.nodeName !== 'IMG') {
-    return;
-    } 
-    instance.show()
-}
+
 function createItemsEl(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`;
-    }).join('');
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+  }).join('');
 }
 
-const imageOriginal = imageOriginalEl(galleryItems)
-function imageOriginalEl(galleryItems) {
-    return galleryItems.map(({ original }) =>{
-        return `<img src = '${original}'>`
-    })
-}
-console.dir(imageOriginal)
+const onContainerClick = (event) => {
+  event.preventDefault();
 
+  if (event.target.classList.contains('gallery')) {
+    return;
+  }
 
+  const source = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="1280">
+  `);
 
+  instance.show();
 
+    const onGalleryClickEsc = (event) => {
+    if (event.code === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', onGalleryClickEsc);
+    }
+  };
 
+  document.addEventListener('keydown', onGalleryClickEsc);
+};
 
-const instance = basicLightbox.create(
-   imageOriginal[3]
-)
-
-
-   
+galleryContainer.addEventListener('click', onContainerClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const addCreateElement = createItemsEl(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', addCreateElement);
+
+function createItemsEl(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+      <img class="gallery__image" 
+      src=${preview}
+      alt=${description}/>
+   </a>
+</li>`;
+  }).join('');
+}
+const ligthBox = new SimpleLightbox('.gallery__link', {
+  captionsData: "alt",
+  captionDelay: 250,
+
+});
